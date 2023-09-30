941490c5b39dde86f6600c5d5f1c65d360ca0187
Preetham Mukkara <mukkarapreetham@gmail.com>
2023-09-30 13:48:57 -0500

    adding JsonProperty taG

--- src/main/kotlin/multiplayer/chess/be/application/domain/Player.kt
+++ src/main/kotlin/multiplayer/chess/be/application/domain/Player.kt
@@ -1,9 +1,12 @@
 package multiplayer.chess.be.application.domain
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class Player(
+    @JsonProperty("playerId")
     val playerId: Int,
+    @JsonProperty("username")
     val username: String,
 )
