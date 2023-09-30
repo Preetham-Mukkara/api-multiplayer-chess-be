5761e53f190641dad100f75d6c1d396592508839
Preetham Mukkara <mukkarapreetham@gmail.com>
2023-09-30 13:26:07 -0500

    adding json serializable to playe class

--- src/main/kotlin/multiplayer/chess/be/application/domain/Player.kt
+++ src/main/kotlin/multiplayer/chess/be/application/domain/Player.kt
@@ -1,5 +1,8 @@
 package multiplayer.chess.be.application.domain
 
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class Player(
     val playerId: Int,
     val username: String,
